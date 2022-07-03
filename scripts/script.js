579e0277b875d072710b42916808829cd01bcf27
Jivesh <jiveshsurajbali@gmail.com>
2022-07-03 20:28:13 +0200

    Add more helper functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,92 @@
 const MOVES = ['rock', 'paper', 'scissors']
 
-while (true) {
+
+function computerPlay(){
+    return MOVES[Math.floor(Math.random()*3)]
+}
+
+
+function playRound(computerSelection, playerSelection) {
+            
+    if (computerSelection == playerSelection) {
+        return 'draw';
+    }
+
+    if (computerSelection == 'rock') {
+        if (playerSelection == 'scissors') { 
+            return 'lose';
+        }
+        else if (playerSelection == 'paper') {
+            return 'win';
+        }
+    }
+
+    else if (computerSelection == 'paper') {
+        if (playerSelection == 'rock') { 
+            return 'lose';
+        }
+        else if (playerSelection == 'scissors') {
+            return 'win';
+        }
+    }
+
+    else if (computerSelection == 'scissors') {
+        if (playerSelection == 'paper') { 
+            return 'lose';
+        }
+        else if (playerSelection == 'rock') {
+            return 'win';
+        }
+    }
+    
+}
+
+
+function getResult(playerScore, computerScore){
+    if (playerScore > computerScore) {
+        console.log(`Player wins ${playerScore} - ${computerScore}!`)
+    }
+    else if (playerScore == computerScore) {
+        console.log(`Game drawn ${playerScore} - ${computerScore}!`)
+    }
+    else if (playerScore < computerScore){
+        console.log(`Player loses ${playerScore} - ${computerScore}!`)
+    }
+}
+
+function game(){
 
     let playerScore = 0;
     let computerScore = 0;
-    
-    for (let i = 1; i != 4; i++) {4
-        function computerPlay(){
-            return MOVES[Math.floor(Math.random()*3)]
+
+    for (i = 1; i != 6; i++){
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Which hand would you like to play (rock, paper, scissors)?");
+        let result = (playRound(computerSelection, playerSelection));
+        
+        if (result == 'win') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+            playerScore++;
         }
 
-
-        function playRound(computerSelection, playerSelection) {
-            
-            if (computerSelection == playerSelection) {
-                alert(`Computer played ${computerSelection}. Round ${i} drawn.`);
-            }
-
-            if (computerSelection == 'rock') {
-                if (playerSelection == 'scissors') { 
-                    computerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} lost.`);
-                }
-                else if (playerSelection == 'paper') {
-                    playerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} won.`);
-                }
-            }
-
-            else if (computerSelection == 'paper') {
-                if (playerSelection == 'rock') { 
-                    computerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} lost.`);
-                }
-                else if (playerSelection == 'scissors') {
-                    playerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} won.`);
-                }
-            }
-
-            else if (computerSelection == 'scissors') {
-                if (playerSelection == 'paper') { 
-                    computerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} lost.`);
-                }
-                else if (playerSelection == 'rock') {
-                    playerScore++;
-                    alert(`Computer played ${computerSelection}. Round ${i} won.`);
-                }
-            }
-            
+        else if (result == 'lose') {
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+            computerScore++;
         }
 
+        else if (result == 'draw') {
+            console.log(`Draw! Both players played ${playerSelection}.`);
+        }
+    }
 
-        var computerSelection = computerPlay();
-        var playerSelection = prompt("What hand would you like to play (rock, paper, scissors)?").toLowerCase();
-        playRound(computerSelection, playerSelection);
+    getResult(playerScore, computerScore);    
+}
 
-    }
-    
-    if (playerScore > computerScore) {
-        alert(`Player wins ${playerScore} - ${computerScore}!`)
-    }
-    else if (playerScore == computerScore) {
-        alert(`Game drawn ${playerScore} - ${computerScore}!`)
-    }
-    else if (playerScore < computerScore){
-        alert(`Player loses ${playerScore} - ${computerScore}!`)
-    }
-    
+while (true){
+
+    game();
+
     if (prompt("Play again (y/n)?").toLowerCase() == 'n') 
        break;
 }
 
-/* No alert when the game is drawn */
